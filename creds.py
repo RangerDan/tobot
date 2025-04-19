63af8db61d5a4b338cad169993fcbf35314158ee
RangerDan <dvhill@gmail.com>
2025-04-18 23:20:19 -0700

    Ports to Bluesky and removes Twitter integration
    Skeet Skeet on repeat

--- creds.py
+++ creds.py
@@ -4,10 +4,8 @@ class TobotCreds:
     creds = {}
 
     def __init__(self):
-        self.creds['TWITTER_CONSUMER_KEY'] = os.environ['TWITTER_CONSUMER_KEY']
-        self.creds['TWITTER_CONSUMER_SECRET'] = os.environ['TWITTER_CONSUMER_SECRET']
-        self.creds['TWITTER_AUTH_TOKEN'] = os.environ['TWITTER_AUTH_TOKEN']
-        self.creds['TWITTER_AUTH_SECRET'] = os.environ['TWITTER_AUTH_SECRET']
+        self.creds['BLUESKY_USERNAME'] = os.environ['BLUESKY_USERNAME']
+        self.creds['BLUESKY_PASSWORD'] = os.environ['BLUESKY_PASSWORD']
         self.creds['NEWS_API_KEY'] = os.environ['NEWS_API_KEY']
 
     def get_cred(self, cred_name):
--- creds_example.sh
+++ creds_example.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
-# Creds for twitter app
-export TWITTER_CONSUMER_KEY="TwitterConsumerKey"
-export TWITTER_CONSUMER_SECRET="TwitterConsumerSecret"
-export TWITTER_AUTH_TOKEN="TwitterAuthToken"
-export TWITTER_AUTH_SECRET="TwitterAuthSecret"
+# Creds for Bluesky
+export BLUESKY_USERNAME="NewValue"
+export BLUESKY_PASSWORD="NewValue"
 # Creds for News API
 export NEWS_API_KEY="NewsApiKey"
